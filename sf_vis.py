ad55fce82ee5445a5a4c13dc0fa6ba643aaaac40
jefsal <159089127+jefsal@users.noreply.github.com>
2026-04-23 23:06:52 -0700

    add display local csv data

--- sf_vis.py
+++ sf_vis.py
@@ -1,33 +1,71 @@
-# File: sf_vis.py
+# File: main.py
 # Author: Jeffrey Salinas
-# 
+# Description : Process local csv data about SF crime, larceny, rendered into a
+#                web page in just python using streamlit
 #
 
-import numpy as np
+# import numpy as np
 import streamlit as st
 import pandas as pd
+import os
 
 # Short intro
 st.title("Crime in San Francisco")
-st.write("There is a popular form of crime that San Francisco has become accostomed to, Larceny Theft and more specifically larceny theft from a vehicle. Better known as \"smash and grab.\" I am curious about what trends, if any, we can identify by pulling public data from police reports and filtering for larceny theft from a vehicle.")
+st.write("There is a popular form of crime that San Francisco has become accostomed to, Larceny Theft and more specifically larceny theft from a vehicle. Better known as \"smash and grab.\"")
+st.write("I am curious about what trends and patterns, if any, we can identify by pulling public data from police reports and filtering for larceny theft from a vehicle.")
+st.divider()
 
 # 2025 larceny theft records 
-st.subheader("2025")
+st.subheader("Smash and Grabs This Month")
 
-# Fetch current month data daily and cache on server for 60s
-@st.cache_data(ttl=60)
-def fetch_current_month_data():
-    # public API endpoint from DataSF
-    url = "https://data.sfgov.org/resource/wg3w-h783.json"
+# declare the absolute csv path for curent month
+current_month_csv_path = os.path.join(
+    os.path.dirname(__file__),
+    "current_month_data.csv",
+)
 
-    today = datetime.now()
-    month_start = now.replace(day=1, hour=0, minute=0)
+# Fetch data from csv and cache
+@st.cache_data
+def load_current_month_data():
+    # read saved data from csv
+    saved_data = pd.read_csv(current_month_csv_path)
 
+    # format incident date time for dt accessor
+    saved_data["incident_date"] = pd.to_datetime(saved_data["incident_date"])
+    saved_data["incident_datetime"] = pd.to_datetime(saved_data["incident_datetime"])
+    saved_data["report_datetime"] = pd.to_datetime(saved_data["report_datetime"])
 
-    # SoQL filter to only fetch from the API the
-    #   subcategory "Larceny - from vehicle"
-    #   incidents after the first day of the month
-    #       AND
-    #   incidents before the the first day of next month
+    return saved_data
+
+st.write("Larceny - from vehicle")
+
+try:
+    # read from local save csv file
+    current_month = load_current_month_data()
+
+    if current_month.empty:
+        st.write("No incidents this month! Must be the first day of the month.")
+    else:
+        # categorize incidents by day for chart readability
+        # dt.normalize() to remove time, group all incidents on the same day
+        current_month_per_day = (
+                current_month.assign(
+                    incident_day=current_month["incident_date"].dt.normalize()
+                )
+                .groupby("incident_day")
+                .size()
+                .reset_index(name="Reports")
+                .sort_values("incident_day").set_index("incident_day")
+        )
+
+        # summarize metrics, verify query to local csv worked
+        st.caption(f"rows loaded from local csv: {len(current_month)}")
+
+        # show monthly chart grouped by day
+        st.subheader("Current month reported incidents grouped by day")
+        st.area_chart(current_month_per_day)
+except FileNotFoundError:
+    st.error("The saved local file was not found")
+except Exception as unexpected_error:
+    st.error(f"Unexpected error found while running: {unexpected_error}") 
     
-
